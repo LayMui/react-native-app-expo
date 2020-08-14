7210968d27acdb07551374689a70ff13e902808d
Lay Mui <mui_ume@hotmail.com>
2020-08-14 20:49:11 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,23 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React , { useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button , ScrollView , FlatList } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
+ 
+  const [ courseGoals, setCourseGoals] = useState([]);
+
+  
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [...currentGoals, 
+      { id: Math.random().toString(), value: goalTitle}]);
+   // setCourseGoals([...courseGoals, enteredGoal]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change Text" onPress={() => setOutputText('The text changed')}/>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <GoalInput onAddGoal={addGoalHandler}/>
+      <FlatList 
+      keyExtractor={(item, index) => item.id }
+        data={courseGoals}
+        renderItem={itemData => <GoalItem title={itemData.item.value}/>}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+   flexDirection: 'row',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
+  
 });
